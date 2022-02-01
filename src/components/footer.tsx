6db2b649bac2e3346199d1d4b1ad5f40fbb68e37
GermanGutierrezCampo <germangutierrezcampo@hotmail.com>
2022-02-01 20:35:21 +0100

    footer rrss icons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,27 @@
+import { Icon } from "@mdi/react";
+import { mdiChevronDoubleUp } from '@mdi/js';
+import { mdiLinkedin } from '@mdi/js';
+import { mdiGithub } from '@mdi/js';
+import { mdiEmail } from '@mdi/js';
+
+
+
+
 export function Footer() {
   return (
     <footer className="relative bg-gray-900 text-gray-400 p-20">
-        <a href="#home" className="bg-pink-700 h-12 w-12 absolute top-0 left-1/2 -mt-6 -ml-6"></a>
+        <a href="#home" className="bg-pink-700 h-12 w-12 absolute top-0 left-1/2 -mt-6 -ml-6 flex items-center justify-center">
+            <Icon path={mdiChevronDoubleUp} color="white"></Icon>
+        </a>
         <div className='flex justify-center'>
-            <div className='bg-gray-700 p-5 mx-10'>
-                in
+            <div className='bg-gray-700 p-2 mx-10 flex items-center justify-center'>
+                <Icon path={mdiLinkedin} color="white" size={1.5}></Icon>
             </div>
-            <div className='bg-gray-700 p-5 mx-10'>
-                Gh
+            <div className='bg-gray-700 p-2 mx-10'>
+                <Icon path={mdiGithub} color="white" size={1.5}></Icon>
             </div>
-            <div className='bg-gray-700 p-5 mx-10'>
-                Mail
+            <div className='bg-gray-700 p-2 mx-10'>
+                <Icon path={mdiEmail} color="white" size={1.5}></Icon>
             </div>
         </div>
         <div>
