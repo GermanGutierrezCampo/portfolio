6c5e5b805345d6e8a11b3acf493c2ab8ac7119d7
GermanGutierrezCampo <germangutierrezcampo@hotmail.com>
2022-02-01 20:08:56 +0100

    mdi + about

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -2,24 +2,32 @@ import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Quality } from './quality';
 import { Skill } from './skill';
+import { mdiCellphoneLink } from '@mdi/js';
+import { mdiSpeedometer } from '@mdi/js';
+import { mdiLightbulbOutline } from '@mdi/js';
+import { mdiRocketLaunch } from '@mdi/js';
+
 
 export function About() {
   const { t } = useTranslation('global');
   const [qualities, setQualities] = useState([
-    {img: '', title: 'title 1', description: 'description 1 description 1 description 1 description 1 description 1 description 1 description 1'},
-    {img: '', title: 'title 2', description: 'description 2 description 2 description 2 description 2 description 2 description 2 description 2'},
-    {img: '', title: 'title 3', description: 'description 3 description 3 description 3 description 3 description 3 description 3 description 3'},
-    {img: '', title: 'title 4', description: 'description 4 description 4 description 4 description 4 description 4 description 4 description 4'}
+    {img: mdiSpeedometer, title: 'about.fast_title', description: 'about.fast_description'},
+    {img: mdiCellphoneLink, title: 'about.responsive_title', description: 'about.responsive_description'},
+    {img: mdiLightbulbOutline, title: 'about.intuitive_title', description: 'about.intuititve_description'},
+    {img: mdiRocketLaunch, title: 'about.dynamic_title', description: 'about.dynamic_description'}
   ]);
   
   const [skills, setSkills] = useState([
-    { name: 'css', percentage: 90 },
-    { name: 'html', percentage: 80 },
-    { name: 'react', percentage: 70 },
-    { name: 'angular', percentage: 60 },
-    { name: 'typescript', percentage: 90 },
-    { name: 'python', percentage: 80 },
-    { name: 'design', percentage: 70 },
+    { name: 'Angular', percentage: 90 },
+    { name: 'React', percentage: 80 },
+    { name: 'TypeScript', percentage: 90 },
+    { name: 'HTML', percentage: 90 },
+    { name: 'CSS', percentage: 80 },
+    { name: 'Python', percentage: 70 },
+    { name: '.NET', percentage: 60 },
+    { name: 'SQL', percentage: 60 },
+    { name: 'Data Science', percentage: 60 },
+    { name: 'UI Design', percentage: 50 },
   ]);
 
   return (
@@ -37,10 +45,9 @@ export function About() {
       <div className="grid lg:grid-cols-2 grid-cols-1 gap-8 mt-32">
         <div className="flex flex-col items-center mt-2">
           <div className="md:w-32 md:h-32 w-28 h-28 bg-teal-400 rounded-full"></div>
-          <strong className="block md:text-3xl text-2xl mt-4"> Who's this guy</strong>
+          <strong className="block md:text-3xl text-2xl mt-4"> {t('about.me_title')}</strong>
           <div className="md:text-lg text-sm text-center">
-              description description description description description description description description description description description description description
-              description description description description description description description description description description description description description
+            {t('about.me_description')}
           </div>
         </div>
         <div className="flex flex-col">
--- src/components/about/quality.tsx
+++ src/components/about/quality.tsx
@@ -1,3 +1,4 @@
+import { Icon } from "@mdi/react";
 import { useTranslation } from "react-i18next";
 
 export function Quality({ item }: any) {
@@ -5,10 +6,12 @@ export function Quality({ item }: any) {
     const { t } = useTranslation('global');
     return (
         <div className="flex flex-col mx-2 items-center">
-            <div className="md:w-32 md:h-32 w-28 h-28 bg-teal-400 rounded-full"></div>
-            <strong className="block md:text-3xl text-2xl mt-4">{ title }</strong>
+            <div className="flex items-center justify-center md:w-32 md:h-32 w-28 h-28 bg-teal-400 rounded-full">
+                <Icon path={img} color="white" size={3}></Icon>
+            </div>
+            <strong className="block md:text-3xl text-2xl mt-4">{ t(title) }</strong>
             <div className="md:text-lg text-sm text-center">
-                { description }
+                { t(description) }
             </div>
         </div>
     );
