c2a3f513eea586fe577a3b54155d01eac5239f4c
GermanGutierrezCampo <germangutierrezcampo@hotmail.com>
2022-02-01 20:35:30 +0100

    landing mdi icons

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,16 +1,20 @@
 import { useTranslation } from 'react-i18next';
 import { DynamicParticles } from './dynamic-particles';
+import { Icon } from "@mdi/react";
+import { mdiArrowRight } from '@mdi/js';
 
 export function Landing() {
   const { t } = useTranslation('global');
   return (
     <div id='home' className='relative bg-gray-800 h-screen'>
-      <div className="absolute grid place-items-center left-0 right-0 h-screen">
-        <div className='text-center text-white text-5xl font-semibold	z-10'>
-          <p>{t('landing.title')} <span className='text-pink-600'>Germán Gutiérrez</span>.</p>
-          <p>{t('landing.subtitle')}</p>
+      <div className="absolute place-items-center left-0 right-0 h-screen text-white">
+        <div className='flex flex-col items-center justify-center h-screen'>
+          <div className='text-center text-5xl font-semibold	z-10'>
+            <p>{t('landing.title')} <span className='text-pink-600'>Germán Gutiérrez</span>.</p>
+            <p>{t('landing.subtitle')}</p>
+          </div>
           <button className='mt-5 text-3xl bg-transparent hover:bg-teal-400 py-2 px-4 border border-white rounded
-          hover:border-transparent'>{t('landing.button')} &rarr;</button>
+          hover:border-transparent flex items-center'>{t('landing.button')} <Icon className='ml-3' path={mdiArrowRight} color="white" size={1}></Icon> </button>
         </div>
 
       </div>
