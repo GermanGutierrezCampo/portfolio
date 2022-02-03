dc2c9c4873ed9d90f39c0c1b28502bd572ade242
GermanGutierrezCampo <germangutierrezcampo@hotmail.com>
2022-02-03 17:44:09 +0100

    footer links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,9 +4,6 @@ import { mdiLinkedin } from '@mdi/js';
 import { mdiGithub } from '@mdi/js';
 import { mdiEmail } from '@mdi/js';
 
-
-
-
 export function Footer() {
   return (
     <footer className="relative bg-gray-900 text-gray-400 p-20">
@@ -14,15 +11,15 @@ export function Footer() {
             <Icon path={mdiChevronDoubleUp} color="white"></Icon>
         </a>
         <div className='flex justify-center'>
-            <div className='bg-gray-700 p-2 mx-10 flex items-center justify-center'>
+            <a href="https://www.linkedin.com/in/ggutierrezcampo/" target="_blank" className='bg-gray-700 p-2 mx-10 cursor-pointer hover:bg-teal-400 transform transition duration-300 hover:scale-90'>
                 <Icon path={mdiLinkedin} color="white" size={1.5}></Icon>
-            </div>
-            <div className='bg-gray-700 p-2 mx-10'>
+            </a>
+            <a href="https://github.com/GermanGutierrezCampo/" target="_blank" className='bg-gray-700 p-2 mx-10 cursor-pointer hover:bg-teal-400 transform transition duration-300 hover:scale-90'>
                 <Icon path={mdiGithub} color="white" size={1.5}></Icon>
-            </div>
-            <div className='bg-gray-700 p-2 mx-10'>
+            </a>
+            <a href="mailto:germangutierrezcampo@gmail.com" target="_blank" className='bg-gray-700 p-2 mx-10 cursor-pointer hover:bg-teal-400 transform transition duration-300 hover:scale-90'>
                 <Icon path={mdiEmail} color="white" size={1.5}></Icon>
-            </div>
+            </a>
         </div>
         <div>
             <span className='text-center mt-10 block'>Germán Gutiérrez</span>
